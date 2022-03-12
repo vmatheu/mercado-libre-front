8af4c5903df72e328345abd51c00ee855732af51
Victor Matheu <victor.matheu@latam.com>
2022-03-12 12:30:12 -0300

    added init config for server webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'mercado-libre-front.js',
+  },
+  devtool: 'source-map',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(less|css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:8882',
+      },
+    },
+    compress: true,
+    inline: true,
+    port: PORT,
+    hot: true,
+    allowedHosts: ['localhost'],
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/public/index.html`,
+      environment: process.env.NODE_ENV || 'production',
+    }),
+  ],
+};
