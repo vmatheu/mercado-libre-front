bb5a1d1608e5918571ee97ccb32791ddb3efcc20
Victor Matheu <victor.matheu@latam.com>
2022-03-12 20:59:20 -0300

    added new style mercadolibre

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { SearchProduct } from 'components/SearchProduct';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => (
-  <SearchProduct />
+  <BrowserRouter>
+    <Route path="/app" component={SearchProduct} />  
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/SearchProduct/index.js
+++ src/components/SearchProduct/index.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import ProductList from 'components/ProductList';
-import Loading from 'components/Loading';
-import productService from './services/productService';
-import './style.sass';
+import React, { useState } from "react";
+import ProductList from "components/ProductList";
+import Loading from "components/Loading";
+import productService from "./services/productService";
+import "./style.sass";
 
-const onlyHaveNumber = (search) => ((/^([0-9]){1,}$/.test(search)));
-const haveLetterAndNumber = (search) => ((/^([0-9,a-z,A-Z,ñ]){4,}$/.test(search)));
-const validateSearch = (search) => (haveLetterAndNumber(search) || onlyHaveNumber(search));
+const onlyHaveNumber = (search) => /^([0-9]){1,}$/.test(search);
+const haveLetterAndNumber = (search) => /^([0-9,a-z,A-Z,ñ]){4,}$/.test(search);
+const validateSearch = (search) =>
+  haveLetterAndNumber(search) || onlyHaveNumber(search);
 
 export const onClick = async (search, setProducts) => {
   if (validateSearch(search)) {
@@ -20,33 +21,47 @@ export const onClick = async (search, setProducts) => {
 
 export const SearchProduct = () => {
   const [data, setProducts] = useState({ products: [], loading: false });
-  return (<div className="container">
-    <Loading loading={data.loading} />
-    <div className="row">
-      <div className="col-xs-12 col-md-5 ">
-        <p>Ingresar busqueda de Productos</p>
+  return (
+    <div className="container">
+      <Loading loading={data.loading} />
+
+      <div className="row">
+        <div className="col-xs-12 col-md-5 ">
+          <p>Ingresar busqueda de Productos</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12 col-md-5">
+          <div>
+            <table>
+              <tr>
+                <td>
+                  <input
+                    id="inputSearchId"
+                    className="form-control mcl-search-product-input-search"
+                  />
+                </td>
+                <td>
+                  <input
+                    width="45px"
+                    src="https://icon-library.com/images/search-icon-jpg/search-icon-jpg-18.jpg"
+                    type="image"
+                    onClick={() => onClick(inputSearchId.value, setProducts)}
+                  />
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="mcl-search-product-bar-space" />
+      </div>
+      <div className="container mcl-search-product-productList">
+        <ProductList products={data.products} />
       </div>
     </div>
-    <div className="row">
-      <div className="col-xs-12 col-md-5">
-        <input
-          id="inputSearchId"
-          className="form-control mcl-search-product-input-search"
-        />
-        <input
-          text="Buscar"
-          type="button"
-          onClick={() => onClick(inputSearchId.value, setProducts)}
-        />
-      </div>
-    </div>
-    <div className="row">
-      <div className="mcl-search-product-bar-space" />
-    </div>
-    <div className="container mcl-search-product-productList">
-      <ProductList products={data.products} />
-    </div>
-  </div>);
+  );
 };
 
 export default { SearchProduct, onClick };
