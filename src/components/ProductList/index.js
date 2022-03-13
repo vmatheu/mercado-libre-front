ab248d7c5340bea687e3d60631392fe5b8f555b0
Victor Matheu <victor.matheu@latam.com>
2022-03-13 11:34:58 -0300

    changed product item style

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { arrayOf, object } from 'prop-types';
 import ProductCard from '../ProductCard';
 
-const ProductList = ({ products }) => {
-  return (<div className="card-deck mb-3 text-center">
+const ProductList = ({products}) => {  
+  return (<div className="text-left">
     {products.map(product => (
       <ProductCard key={product.id} {...product} />
     ))}
-  </div>);
-};
+  </div>);   
+}
 
 ProductList.propTypes = {
   products: arrayOf(object).isRequired,
